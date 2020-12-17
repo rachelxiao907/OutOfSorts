c8940a33829d7a3855e9869f3c95a408f717a940
Rachel Xiao <rachelxiao907@gmail.com>
2020-12-16 23:38:35 -0500

    Changed bubbleSort to stop checking when array is sorted

--- Sorts.java
+++ Sorts.java
@@ -2,14 +2,20 @@ public class Sorts{
   //Bubble sort of an int array.
   //@postcondition The array will be modified such that the elements will be in increasing order.
   public static void bubbleSort(int[] data) {
-    for (int i = 0; i < data.length; i++) {
-      for (int j = 0; j < data.length - i - 1; j++) {
-        if (data[j] > data[j+1]) {
-          int old = data[j];
-          data[j] = data[j+1];
-          data[j+1] = old;
+    boolean sorted = false;
+    while (sorted == false) {
+      sorted = true;
+      int end = 0;
+      for (int i = 0; i < data.length - 1 - end; i++) {
+        if (data[i] > data[i+1]) {
+          sorted = false;
+          int old = data[i];
+          data[i] = data[i+1];
+          data[i+1] = old;
         }
       }
+      end++;
     }
   }
+
 }

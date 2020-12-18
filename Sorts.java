8c6109e8c50e245a0797064039fdb66a7af33047
Rachel Xiao <rachelxiao907@gmail.com>
2020-12-17 20:05:53 -0500

    Modified bubbleSort to use for loops because it seems more logical to me

--- Sorts.java
+++ Sorts.java
@@ -1,11 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 public class Sorts{
   //Bubble sort of an int array.
   //@postcondition The array will be modified such that the elements will be in increasing order.
   public static void bubbleSort(int[] data) {
     boolean sorted = false;
-    for (int end = 1; !sorted; end++) {
+    for (int end = 0; !sorted; end++) {
       sorted = true;
-      for (int i = 0; i < data.length - end; i++) {
+      for (int i = 0; i < data.length - 1 - end; i++) {
         if (data[i] > data[i+1]) {
           sorted = false;
           int old = data[i];
